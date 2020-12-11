fcca5ca3c6e73a264f288f6b6b7c3f64162ea152
Prince Mathew <12183859+princemathew@users.noreply.github.com>
2020-12-11 16:45:14 +0530

    pdf doc support

--- src/crack.js
+++ src/crack.js
@@ -9,12 +9,16 @@ function crack(){
         /*for building uncomment below line */
         //var perl = require('child_process').spawn('perl',[path.join(__dirname, '../..', '/src/run/pdf2john.pl'),filepath]);
         /*for building comment below line */
-
-        var checkpotfile = "src/run/hashcat.bin"+" -m "+hashtype+" "+hash+" --show";
+        require('fs').writeFileSync('src/run/hash.txt',hash)
+        if(hashtype == '9400' || hashtype == '9500'||hashtype == '9600'||hashtype == '10400'||hashtype == '10500')
+            hash = "--username src/run/hash.txt"
+        else 
+            hash = "src/run/hash.txt"
+        var checkpotfile = "src/run/hashcat.bin -m "+hashtype+" "+hash+" --show";
         var potfile = require('child_process').execSync(checkpotfile);
         var cracked=0
         if(potfile.toString('utf8')){
-            document.getElementById('result').innerHTML=potfile.toString('utf8')
+            document.getElementById('result').value=potfile.toString('utf8')
             document.getElementById('cracksub').disabled = false;
 
         }
@@ -25,17 +29,17 @@ function crack(){
                 var hashc = "src/run/hashcat.bin"+" -m "+hashtype+" -a 3 "+hash+" -1 "+chars+" "+charset+ " --quiet";
                 var hashcat = require('child_process').exec(hashc);
         
-                document.getElementById('result').innerHTML="processing..."
+                document.getElementById('result').value="processing..."
                
     
                 hashcat.stdout.on('data', function (data) {
-                    document.getElementById('result').innerHTML = data.toString('utf8');
-                    cracked ==1;
+                    document.getElementById('result').value = data.toString('utf8');
+                    cracked =1;
                 });
                 hashcat.on('close', () => {
                     document.getElementById('cracksub').disabled = false;
-                    if(cracked ==1){
-                        document.getElementById('result').innerHTML="keyspace exhausted"
+                    if(cracked ==0){
+                        document.getElementById('result').value="keyspace exhausted"
                     }
                 });
 
